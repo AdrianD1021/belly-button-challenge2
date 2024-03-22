10100912204881ffc573f583006fc9e00733c430
AdrianD1021 <adriandd1021@gmail.com>
2024-03-21 20:23:36 -0400

    3rd commit

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,124 +1,114 @@
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 
-function init() {
-  // Creating the dropdown
-  var selector = d3.select("#selDataset");
-  
-  // Filling the dropdown with subject IDs from the list of sample Names
-    d3.json("data/samples.json").then((data) => {
-      var subjectIds = data.names;
-      subjectIds.forEach((id) => {
-        selector
-        .append("option")
-        .text(id)
-        .property("value", id);
-      });
-    
-    // Use the first subject ID from names
-    const firstSubject = subjectIds[0];
-    updateCharts(firstSubject);
-    updateMetadata(firstSubject);
-  });
+const dataPromise = d3.json(url);
+    console.log("Data Promise: ", dataPromise):
+
+d3.json(url).then(function(data){
+    console.log(data);
+})
+
+//Variables and getting data from JSON for charts
+
+var samples;
+var metadata;
+
+d3.json(url).then(function (data) {
+    let selector = d3.select("#selDataset"):
+    metadata = data.metadata;
+    samples = data.samples;
+    data.names.forEach((id) => {
+        selector.append("option").text(id).property("value",id);
+    });
+    Metadata(metadata[0]);
+    Hbarchart(samples[0]);
+    Bubblechart(samples[0]);
+
+});
+
+function OptionChange (value) {
+    const SelectedID = samples.find((item) => item.id == value);
+    const Demographics = metadata.find(item) => item.id == value);
+
+    Metadata(Demographics);
+
+    Hbarchart(SelectedID);
+
+    Bubblechart(SelectedID);
+
 }
 
+//Demographic Information
 
+function Metadata (Demographics) {
+    let demoSelect = d3.select("#sample-metadata");
 
-function updateMetadata(sample) {
-  d3.json("data/samples.json").then((data) => {
-      var metadata = data.metadata;
-      var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
-      var result = filterArray[0];
-      var metaPanel = d3.select("#sample-metadata");
-      metaPanel.html("");
-      Object.entries(result).forEach(([key, value]) => {
-          metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
-      })
-  
-// Filling in data for Gauge Chart
-  var data = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      marker: {size: 28, color:'850000'},
-      value: result.wfreq,
-      title: 'Belly Button Washing Frequency<br> Scrubs per Week',
-      titlefont: {family: '"Palatino Linotype", "Book Antiqua", Palatino, serif'},
-      type: "indicator",
-      mode: "gauge+number"
-    }
-  ];
-  // Setting the Layout
-
-  var layout = {
-    width: 450,
-     height: 400,
-     margin: { t: 25, r: 25, l: 25, b: 25 },
-     line: {
-     color: '600000'
-     },
-     paper_bgcolor: "#a5bdc6",
-     font: { color: "#85541d", family: "Serif" }
-   };
-
-  
-  Plotly.newPlot("gauge", data, layout);
-
-  });
+    demoSelect.html(
+        `id: ${Demographics.id} <br>
+        ethnicity: ${Demographics.ethnicity} <br>
+    gender: ${Demographics.gender} <br>
+    age: ${Demographics.age} <br>
+    location: ${Demographics.location} <br>
+    bbtype: ${Demographics.bbtype} <br>
+    wfreq: ${Demographics.wfreq}`
+        ):
 }
 
+function Hbarchart (SelectedID) {
+    let xaxis = SelectedID.sample_values.slice(0,10).reverse();
+    let yaxis = SelectedID.otu_ids
+    .slice(0,10)
+    .reverse()
+    .map((item) => `OTU ${item}`;
+    let text = SelectedID.otu_labels.slice(0,10).reverse();
 
-function updateCharts(sample) {    
-  d3.json("data/samples.json").then((data) => {
-  var samples = data.samples;
-  var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
-  var result = filterArray[0];
-  var sample_values = result.sample_values;
-  var otu_ids = result.otu_ids;
-  var otu_labels = result.otu_labels;   
-  // Bubble Chart
-  var trace1 = {
-      x: otu_ids,
-      y: sample_values,
-      text: otu_labels,
-      mode: 'markers',
-      marker: {
-      size: sample_values,
-      color: otu_ids,
-      colorscale:"Electric"
-      }
-  };
-  var data = [trace1];
-  var layout = {
-      title: 'Bacteria Cultures per Sample',
-      showlegend: false,
-      hovermode: 'closest',
-      xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
-      margin: {t:30}
-  };
-  Plotly.newPlot('bubble', data, layout); 
-  // Bar Chart
-  var trace1 = {
-      x: sample_values.slice(0,10).reverse(),
-      y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
-      text: otu_labels.slice(0,10).reverse(),
-      name: "Greek",
-      type: "bar",
-      orientation: "h"
-  };
-  var data = [trace1];
-  var layout = {
-      title: "Top Ten OTUs for Individual " +sample,
-      margin: {l: 100, r: 100, t: 100, b: 100}
-  };
-  Plotly.newPlot("bar", data, layout);  
-  });
+    Barchart = {
+        x: xaxis,
+        y: yaxis,
+        text: text,
+        type: "bar",
+        orientation: "h",
+    };
+
+    let chart = [Barchart];
+
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height:500,
+        width:600,
+    };
+
+    Plotly.Newplot("bar",chart,layout);
 }
 
+function Bubblechart(SelectedID) {
+    let xaxis = SelectedID.otu_ids;
+    let yaxis = SelectedID.sample_values;
+    let markersize = SelectedID.otu_ids;
+    let text = SelectedID.otu_labels;
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  updateCharts(newSample);
-  updateMetadata(newSample);
+    bubble = {
+        x: xaxis,
+        y: yaxis,
+        text: text,
+        mode: "markers",
+        marker: {
+            color: color,
+            colorscale: "Pastel",
+            size: markersize,
+        },
+        type: "scatter",
+    };
+    let chart = [bubble];
+
+    let layout={
+        xaxis:{
+            title: {text: "OTU ID"},
+        },
+    };
+    Plotly.Newplot("bubble",chart,layout);
 }
-
-// Initialize the dashboard
-init();
